b025b0d63abed35331fe3834c5bbaf8ef4854693
Neloy-SWE <taufiqneloy.swe@gmail.com>
2022-07-09 20:23:12 +0600

    use of factory keyword

---
+++ lib/app/app.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget{
+  //const MyApp({Key? key}): super(key: key);
+
+  int appState = 0;
+
+  // private name constructor
+  MyApp._internal();
+
+  // single instance -- singleton
+  static final MyApp instance = MyApp._internal();
+
+
+  factory MyApp() => instance;
+
+
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp>{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold();
+  }
+}
