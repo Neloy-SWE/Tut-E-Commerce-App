dfae06bb7e90c32894f608dc8ebbfe81e19a9e77
Neloy-SWE <taufiqneloy.swe@gmail.com>
2022-07-18 16:20:38 +0600

    timer added in splash screen

--- lib/presentation/resources/color_manager.dart
+++ lib/presentation/resources/color_manager.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 class ColorManager {
-  static Color primary = HexColor.fromHex("#0078EF");
+  static Color primary = HexColor.fromHex("#ED9728");
   static Color darkGray = HexColor.fromHex("#525252");
   static Color gray = HexColor.fromHex("#737477");
   static Color lightGray = HexColor.fromHex("#9E9E9E");
-  static Color primaryOpacity70 = HexColor.fromHex("#B39E9E9E");
+  static Color primaryOpacity70 = HexColor.fromHex("#B3ED9728");
 
-  static Color darkPrimary = HexColor.fromHex("#B39E9E9E");
+  static Color darkPrimary = HexColor.fromHex("#d17d11");
   static Color gray1 = HexColor.fromHex("#707070");
   static Color gray2 = HexColor.fromHex("#797979");
   static Color white = HexColor.fromHex("#FFFFFF");
