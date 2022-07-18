720f6744ad28987ba00d781ef4c720394efda1f8
Neloy-SWE <taufiqneloy.swe@gmail.com>
2022-07-18 18:57:09 +0600

    onboarding working on

--- lib/presentation/onboarding/onboarding.dart
+++ lib/presentation/onboarding/onboarding.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:tut_e_commerce_app/presentation/resources/assets_manager.dart';
 import 'package:tut_e_commerce_app/presentation/resources/color_manager.dart';
+import 'package:tut_e_commerce_app/presentation/resources/string_manager.dart';
 
 class OnBoardingView extends StatefulWidget {
   const OnBoardingView({Key? key}) : super(key: key);
@@ -9,6 +11,17 @@ class OnBoardingView extends StatefulWidget {
 }
 
 class _OnBoardingViewState extends State<OnBoardingView> {
+
+  late final List<SliderObject> _list = _getSlider();
+
+  List<SliderObject> _getSlider(){
+    return [
+      SliderObject(AppStrings.onBoardingTitle1, AppStrings.onBoardingSubTitle1, ImageAssets.onBoardingLogo1),
+      SliderObject(AppStrings.onBoardingTitle2, AppStrings.onBoardingSubTitle2, ImageAssets.onBoardingLogo2),
+      SliderObject(AppStrings.onBoardingTitle3, AppStrings.onBoardingSubTitle3, ImageAssets.onBoardingLogo3),
+      SliderObject(AppStrings.onBoardingTitle4, AppStrings.onBoardingSubTitle4, ImageAssets.onBoardingLogo4),
+    ];
+}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,4 +33,13 @@ class _OnBoardingViewState extends State<OnBoardingView> {
       ),
     );
   }
+}
+
+
+class SliderObject {
+  String title;
+  String subTitle;
+  String image;
+
+  SliderObject(this.title, this.subTitle, this.image);
 }
