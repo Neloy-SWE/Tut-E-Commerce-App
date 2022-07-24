67c3c509fa0e56521d0cf17af246e19d7127e707
Neloy-SWE <taufiqneloy.swe@gmail.com>
2022-07-24 12:12:29 +0600

    extension added for customer response, contact response, authentication response

---
+++ lib/data/mapper/mapper.dart
@@ -0,0 +1,35 @@
+import 'package:tut_e_commerce_app/app/extensions.dart';
+import 'package:tut_e_commerce_app/data/responses/responses.dart';
+import 'package:tut_e_commerce_app/domain/model.dart';
+
+const EMPTY = "";
+const ZERO = 0;
+
+extension CustomerResponseMapper on CustomerResponse? {
+  Customer toDomain() {
+    return Customer(
+      this?.id?.orEmpty() ?? EMPTY,
+      this?.name?.orEmpty() ?? EMPTY,
+      this?.numOfNotifications?.orZero() ?? ZERO,
+    );
+  }
+}
+
+extension ContactsResponseMapper on ContactsResponse? {
+  Contacts toDomain() {
+    return Contacts(
+      this?.email?.orEmpty() ?? EMPTY,
+      this?.phone?.orEmpty() ?? EMPTY,
+      this?.link?.orEmpty() ?? EMPTY,
+    );
+  }
+}
+
+extension AuthenticationResponseMapper on AuthenticationResponse? {
+  Authentication toDomain() {
+    return Authentication(
+      this?.customer?.toDomain(),
+      this?.contact?.toDomain(),
+    );
+  }
+}
