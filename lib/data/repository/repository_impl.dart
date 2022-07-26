7d812022a182fb7a197b0894d7bb3d48a5c9502c
Neloy-SWE <taufiqneloy.swe@gmail.com>
2022-07-26 08:05:40 +0600

    dio factory added

--- lib/data/repository/repository_impl.dart
+++ lib/data/repository/repository_impl.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:tut_e_commerce_app/data/data_sources/remote_data_source.dart';
 import 'package:tut_e_commerce_app/data/mapper/mapper.dart';
+import 'package:tut_e_commerce_app/data/network/error_handler.dart';
 import 'package:tut_e_commerce_app/data/network/failure.dart';
 import 'package:tut_e_commerce_app/data/network/network_info.dart';
 import 'package:tut_e_commerce_app/data/request/request.dart';
@@ -17,15 +18,19 @@ class RepositoryImpl extends Repository {
   Future<Either<Failure, Authentication>> login(
       LoginRequest loginRequest) async {
     if (await _networkInfo.isConnected) {
-      final response = await _remoteDataSource.login(loginRequest);
-      if (response.status == 0) {
-        return Right(response.toDomain());
-      } else {
-        return Left(Failure(
-            409, response.message ?? "we have biz error logic from API side"));
+      try {
+        final response = await _remoteDataSource.login(loginRequest);
+        if (response.status == ApiInternalStatus.SUCCESS) {
+          return Right(response.toDomain());
+        } else {
+          return Left(Failure(response.status ?? ApiInternalStatus.FAILURE,
+              response.message ?? ResponseMessage.DEFAULT));
+        }
+      } catch (error) {
+        return (Left(ErrorHandler.handle(error).failure));
       }
     } else {
-      return Left(Failure(501, "Please check your internet connection"));
+      return Left(DataSource.NO_INTERNET_CONNECTION.getFailure());
     }
   }
 }
